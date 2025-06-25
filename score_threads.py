44d7ca151e3cbf7df328f93bba60ddbafa20c600
Siddhant Mohan <siddhantmohan1110@gmail.com>
2025-06-24 23:42:03 -0400

    used pandas vectorization

--- score_threads.py
+++ score_threads.py
@@ -5,26 +5,7 @@ import numpy as np
 df_loaded = pd.read_csv("threads.csv")
 
 # Repeat the vectorized score calculation on the loaded DataFrame
-
-# Extract columns as arrays
-likes = df_loaded['likes'].to_numpy()
-replies = df_loaded['replies'].to_numpy()
-reposts = df_loaded['reposts'].to_numpy()
-age_minutes = df_loaded['age_minutes'].to_numpy()
-
-# Compute decay
-age_decay = np.exp(-age_minutes / 120)
-
-# Compute score parts
-likes_part = likes * 2
-replies_part = replies * 3
-reposts_part = reposts * 2.5 * age_decay
-
-# Compute final score
-score = likes_part + replies_part + reposts_part
-
-# Append score to DataFrame
-df_loaded['score'] = score
+df_loaded['score'] = (df_loaded['likes']*2) + (df_loaded['replies']*3) + (df_loaded['reposts']*2.5) * np.exp(-df_loaded['age_minutes']/120)
 
 # Save updated DataFrame
 csv_path_scored_from_loaded = "threads_scored.csv"
