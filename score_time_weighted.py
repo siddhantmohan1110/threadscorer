c54b9c34f5a29694eda2227336cfb65a0f362877
Siddhant Mohan <44978626+siddhantmohan1110@users.noreply.github.com>
2025-07-17 08:58:35 -0400

    Added logistic decay function

--- score_time_weighted.py
+++ score_time_weighted.py
@@ -12,3 +12,15 @@ def calculate_time_weighted_score(
         lambda g: (g['time_weight'] * g[score_col]).sum() * 100 / g[score_col].sum()
     ).reset_index(name='time_weighted_score')
     return weighted 
+
+def calculate_time_weighted_score_logistic(
+    df: pd.DataFrame,
+    period_col='period', score_col='normalized_score',
+    b=2.4, a=0.1
+) -> pd.DataFrame:
+    df = df.copy()
+    df['time_weight'] = 1/(1+np.exp(df[period_col] - b)/a)
+    weighted = df.groupby('thread_id').apply(
+        lambda g: (g['time_weight'] * g[score_col]).sum() * 100 / g[score_col].sum()
+    ).reset_index(name='time_weighted_score')
+    return weighted 
